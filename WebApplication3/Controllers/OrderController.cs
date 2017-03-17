f20e69888f3f933be5d50f659122c6eb6a92f651
user <user@MA324-35>
2017-03-17 13:04:40 +0800

    加入專案檔案。

---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            Models.OrderService OrderService=new Models.OrderService();
+            var order=OrderService.GetOrderById("111");
+            ViewBag.CustId=order.CustId;
+            ViewBag.CustName=order.CustName;
+            return View();
+        }
+	}
+}
